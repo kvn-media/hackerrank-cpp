f37050debff5f277f2929ff7cf7e9349ad20dfa7
kvn-media <kevin.subagio@gmail.com>
2022-08-18 11:04:07 +0700

    first commit

---
+++ basicDataType.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    string s;
+    for(int i = 0; i<5; i++){
+        cin >> s;
+        cout << s << endl;
+    }
+    return 0;
+}
